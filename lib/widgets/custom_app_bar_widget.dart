93910e70266dfeb54d5b7ff85e47b8d16a26a872
Bob Taylor <bob.taylor.mba@gmail.com>
2026-09-15 16:36:45 -0700

    Added assets and images along with the custom app bar widget

---
+++ lib/widgets/custom_app_bar_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final double getToolBarHeight;
+
+  const CustomAppBarWidget({
+    super.key,
+    required this.title,
+    required this.getToolBarHeight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    double iconSize = (screenWidth * 0.06).clamp(50.0, 60.0);
+
+    return AppBar(
+      surfaceTintColor: Colors.transparent,
+      elevation: 0,
+      toolbarHeight: getToolBarHeight,
+      scrolledUnderElevation: 0,
+      automaticallyImplyLeading: true,
+      iconTheme: IconThemeData(size: iconSize),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.displaySmall,
+        softWrap: true,
+        textAlign: TextAlign.center,
+        maxLines: null,
+        overflow: TextOverflow.visible,
+      ),
+      actions: const [],
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/blue_sky_background_app_bar.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(getToolBarHeight);
+}
